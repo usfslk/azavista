12c62c0ba909bed6c67b9b1fb5fab6c85e5a7d75
usfslk <macbookair@usfslks-MacBook-Air.local>
2019-07-11 23:49:55 +0100

    .btn

--- src/App.js
+++ src/App.js
@@ -9,21 +9,21 @@ export default class Build extends Component {
     super(props)
     this.state = {
       likes: 100,
-      dislikes: 100,
+      dislikes: 100
     }
   }
 
   like = () => {
     this.setState({ likes: this.state.likes + 1, liked: true })
     if (this.state.disliked) {
-      this.prevDislike();
+      this.prevDislike()
     }
   }
 
   dislike = () => {
     this.setState({ dislikes: this.state.dislikes + 1, disliked: true })
     if (this.state.liked) {
-      this.prevLike();
+      this.prevLike()
     }
   }
 
@@ -32,30 +32,40 @@ export default class Build extends Component {
   }
 
   prevDislike = () => {
-    this.setState({  dislikes: this.state.dislikes - 1, disliked: false })
+    this.setState({ dislikes: this.state.dislikes - 1, disliked: false })
   }
 
   render () {
     let { likes, dislikes, liked, disliked } = this.state
     return (
       <div className='App'>
-          {liked ? (
-            <button style={{ backgroundColor: 'darkblue', color: '#fff' }}
-            onClick={this.prevLike}>
-              Like | {likes}
-            </button>
-          ) : (
-            <button onClick={this.like}>Like | {likes}</button>
-          )}
-          <br />
-          {disliked ? (
-            <button style={{ backgroundColor: 'darkblue', color: '#fff' }}
-             onClick={this.prevDislike}>
-              Dislike | {dislikes}
-            </button>
-          ) : (
-            <button onClick={this.dislike}>Dislike | {dislikes}</button>
-          )}
+        {liked ? (
+          <button
+            style={{ backgroundColor: 'darkblue', color: '#fff' }}
+            onClick={this.prevLike}
+            className='btn'
+          >
+            Like | {likes}
+          </button>
+        ) : (
+          <button className='btn' onClick={this.like}>
+            Like | {likes}
+          </button>
+        )}
+        <br />
+        {disliked ? (
+          <button
+            className='btn'
+            style={{ backgroundColor: 'darkblue', color: '#fff' }}
+            onClick={this.prevDislike}
+          >
+            Dislike | {dislikes}
+          </button>
+        ) : (
+          <button className='btn' onClick={this.dislike}>
+            Dislike | {dislikes}
+          </button>
+        )}
       </div>
     )
   }
